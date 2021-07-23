ddf4fca7d749905099d16044d76442da84e752a5
Sophie Shaw <sophie.shaw@wales.nhs.uk>
2021-07-23 13:19:38 +0100

    Adding second check page and setting sample info section

--- acmg_db/views/cnv_second_check_views.py
+++ acmg_db/views/cnv_second_check_views.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 from django.template.loader import render_to_string
 from django.http import HttpResponse
 
-from acmg_db.forms import SampleInfoFormSecondCheck, VariantInfoFormSecondCheck, FinaliseClassificationSecondCheckForm, TranscriptFormSecondCheck
+from acmg_db.forms import CNVSampleInfoForm, VariantInfoFormSecondCheck, FinaliseClassificationSecondCheckForm, TranscriptFormSecondCheck
 from acmg_db.models import *
 
 #--------------------------------------------------------------------------------------------------
@@ -84,69 +84,55 @@ def cnv_second_check(request, pk):
 
 	PANEL_OPTIONS = [(str(panel.pk), panel) for panel in Panel.objects.all().order_by('panel')]
 
-	classification = get_object_or_404(Classification, pk=pk)
+	cnv = get_object_or_404(CNV, pk=pk)
 
 	# Assign second check to first person to click the link
-	if classification.user_second_checker == None:
+	if cnv.user_second_checker == None:
 
-		classification.user_second_checker = request.user
-		classification.save()
+		cnv.user_second_checker = request.user
+		cnv.save()
 
 	#reject if wrong status or user
-	if classification.status != '1' or request.user != classification.user_second_checker:
+	if cnv.status != '1' or request.user != cnv.user_second_checker:
 
 		raise PermissionDenied(f'You do not have permission to perform the second check. It is assigned to {request.user}')
 
 	else:
 		# Get data to render form
-		variant = classification.variant
 
-		previous_classifications = Classification.objects.filter(variant=variant,genome=classification.sample.genome, status__in=['2', '3']).exclude(pk=classification.pk).order_by('-second_check_date')
-		previous_full_classifications = previous_classifications.filter(genuine='1').order_by('-second_check_date')
+#		previous_classifications = Classification.objects.filter(variant=variant,genome=classification.sample.genome, status__in=['2', '3']).exclude(pk=classification.pk).order_by('-second_check_date')
+#		previous_full_classifications = previous_classifications.filter(genuine='1').order_by('-second_check_date')
 
-		answers = ClassificationAnswer.objects.filter(classification=classification).order_by('classification_question__order')
-		comments = UserComment.objects.filter(classification=classification, visible=True)
+#		answers = ClassificationAnswer.objects.filter(classification=classification).order_by('classification_question__order')
+#		comments = UserComment.objects.filter(classification=classification, visible=True)
 
-		result_first = classification.display_first_classification()
-		result_second = classification.display_final_classification()
+		result_first = cnv.display_first_classification()
+		result_second = cnv.display_final_classification()
 
 		if result_second == 'VUS - contradictory evidence provided':
 
 			result_second = 'Contradictory evidence provided'
 
-		transcript = classification.selected_transcript_variant.transcript
-
-		refseq_options = TranscriptVariant.objects.filter(variant=variant)
-		fixed_refseq_options = []
-
-		# Get relevant options for the variant transcripts
-		for transcript_var in refseq_options:
-
-			fixed_refseq_options.append((transcript_var.pk, transcript_var.transcript.name + ' - ' + transcript_var.transcript.gene.name + ' - ' + transcript_var.consequence))
+		
 
 		# make empty instances of forms
-		sample_form = SampleInfoFormSecondCheck(classification_pk=classification.pk, options=PANEL_OPTIONS)
-		transcript_form = TranscriptFormSecondCheck(classification_pk=classification.pk, options=fixed_refseq_options)
-		variant_form = VariantInfoFormSecondCheck(classification_pk=classification.pk, options=fixed_refseq_options)
-		finalise_form = FinaliseClassificationSecondCheckForm(classification_pk=classification.pk)
+		sample_form = CNVSampleInfoForm(request.POST)
+#		finalise_form = FinaliseClassificationSecondCheckForm(classification_pk=classification.pk)
 
 		# dict of data to pass to view
 		context = {
-			'classification': classification,
-			'variant': variant,
-			'previous_classifications': previous_classifications,
-			'previous_full_classifications': previous_full_classifications,
-			'answers': answers,
-			'comments': comments,
+			'cnv': cnv,
+#			'previous_classifications': previous_classifications,
+#			'previous_full_classifications': previous_full_classifications,
+#			'answers': answers,
+#			'comments': comments,
 			'result_first': result_first,
 			'result_second': result_second,
 			'sample_form': sample_form,
-			'transcript_form': transcript_form,
-			'variant_form': variant_form,
-			'finalise_form': finalise_form,
+#			'finalise_form': finalise_form,
 			'warn': []
 		}
-		
+			
 		#-----------------------------------------------
 		# if a form is submitted
 		if request.method == 'POST':
@@ -154,79 +140,24 @@ def cnv_second_check(request, pk):
 			# SampleInfoForm
 			if 'affected_with' in request.POST:
 
-				sample_form = SampleInfoFormSecondCheck(request.POST, classification_pk=classification.pk, options=PANEL_OPTIONS)
+				sample_form = CNVSampleInfoForm(request.POST)
 
 				# load in data
 				if sample_form.is_valid():
 
 					cleaned_data = sample_form.cleaned_data
-					sample = classification.sample
+					sample = cnv.sample
 					sample.affected_with =  cleaned_data['affected_with']
-					sample.other_changes = cleaned_data['other_changes']
+					sample.cyto = cleaned_data['cyto_ID']
+					sample.platform = cleaned_data['platform']
 					sample.save()
 					
 				
 				# reload dict variables for rendering
-				context['classification'] = classification
-				context['sample_form'] = SampleInfoFormSecondCheck(classification_pk=classification.pk, options=PANEL_OPTIONS)
-
-			# TranscriptForm
-			if 'select_transcript' in request.POST:
-				transcript_form = TranscriptFormSecondCheck(request.POST, classification_pk=classification.pk, options=fixed_refseq_options)
-				if transcript_form.is_valid():
-					cleaned_data = transcript_form.cleaned_data
-					select_transcript = cleaned_data['select_transcript']
-					selected_transcript_variant = get_object_or_404(TranscriptVariant, pk=select_transcript)
-					classification.selected_transcript_variant = selected_transcript_variant
-					classification.save()
-				# reload dict variables for rendering
-				context['classification'] = classification
-				context['transcript_form'] = TranscriptFormSecondCheck(classification_pk=classification.pk, options=fixed_refseq_options)
-				context['variant_form'] = VariantInfoFormSecondCheck(classification_pk=classification.pk, options=fixed_refseq_options)
-
-
-			# VariantInfoForm
-			if 'inheritance_pattern' in request.POST:
-
-				variant_form = VariantInfoFormSecondCheck(request.POST, classification_pk = classification.pk, options=fixed_refseq_options)
-
-				if variant_form.is_valid():
-
-					cleaned_data = variant_form.cleaned_data
-
-					classification.is_trio_de_novo = cleaned_data['is_trio_de_novo']
-
-					genotype = cleaned_data['genotype']
-
-					if genotype == 'Het':
-						genotype = 1
-
-					elif genotype == 'Hom':
-						genotype = 2
-
-					elif genotype == 'Hemi':
-						genotype = 3
-
-					elif genotype == 'Mosaic':
-						genotype = 4
-
-					else:
-						genotype = None
-
-					classification.genotype = genotype
-					
-					classification.save()
-
-					# genes section
-					gene = classification.selected_transcript_variant.transcript.gene
-					gene.inheritance_pattern = cleaned_data['inheritance_pattern']
-					gene.conditions = cleaned_data['conditions']
-					gene.save()
-
-				# reload dict variables for rendering
-				context['classification'] = classification
-				context['variant_form'] = VariantInfoFormSecondCheck(classification_pk=classification.pk, options=fixed_refseq_options)
-
+				context['cnv'] = get_object_or_404(CNV, pk=pk)
+				context['sample_form'] = CNVSampleInfoForm(request.POST)
+		"""
+			
 			# FinaliseClassificationForm
 			if 'final_classification' in request.POST:
 
@@ -294,4 +225,6 @@ def cnv_second_check(request, pk):
 						))
 
 			return render(request, 'acmg_db/second_check.html', context)
-		return render(request, 'acmg_db/second_check.html', context)
+			"""
+			
+		return render(request, 'acmg_db/cnv_second_check.html', context)
