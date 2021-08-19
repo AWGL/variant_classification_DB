64911c2a58b9e68eb00152026700a0d259a9228b
Sophie Shaw <sophie.shaw@wales.nhs.uk>
2021-08-19 11:57:23 +0100

    Updating unit tests to reflect changes in SNP models

--- acmg_db/tests/test_variant_utils.py
+++ acmg_db/tests/test_variant_utils.py
@@ -1,6 +1,7 @@
 import unittest
 from acmg_db.utils.variant_utils import *
 from django.conf import settings
+from django.test import TestCase
 
 
 class TestVariantUtils(unittest.TestCase):
@@ -8,21 +9,23 @@ class TestVariantUtils(unittest.TestCase):
 
 	def test_process_variant_input(self):
 
-		input1 = '17:41197732GGGGGATGC>ATGC'
-		input2 = '17:41197732AAAAG>ATTTTT'
-		input3 = '1:41197732G>A'
-		input4 = 'X:41197732G>AAA'
-		input5 = 'M:41197732G>A'
-		input6 = 'Y:41197732G>A'
-		input7 = '13:4GTGAC>ATGC'
+		input1 = ('17:41197732GGGGGATGC>ATGC')
+		input2 = ('17:41197732AAAAG>ATTTTT')
+		input3 = ('1:41197732G>A')
+		input4 = ('X:41197732G>AAA')
+		input5 = ('M:41197732G>A')
+		input6 = ('Y:41197732G>A')
+		input7 = ('13:4GTGAC>ATGC')
+		ref1 = ('GRCh37')
+		ref2 = ('GRCh38')
 
-		self.assertEqual(process_variant_input(input1), ['e619796bddad0e522b9a127c3bea7906492173156fe5d566982ac06294a1c6b1', '17', '41197732', 'GGGGGATGC', 'ATGC', '17-41197732-GGGGGATGC-ATGC'])
-		self.assertEqual(process_variant_input(input2), ['2bd177ad77d08a7d8ee305a6e5c8912a464186bddeaf0b3efe3557106c44d45d', '17', '41197732', 'AAAAG', 'ATTTTT', '17-41197732-AAAAG-ATTTTT'])
-		self.assertEqual(process_variant_input(input3), ['1803ac07aaa1a21494fde4ddf6b2f154ac569e2dbb195c0d4428b21a74580e73', '1', '41197732', 'G', 'A', '1-41197732-G-A'])
-		self.assertEqual(process_variant_input(input4), ['3b71486b09ad5b9c88a5604c6847eab432f1e419da68e01f9f49d628073ea567', 'X', '41197732', 'G', 'AAA', 'X-41197732-G-AAA'])
-		self.assertEqual(process_variant_input(input5), ['8e6e0816a10135d2d295f2b42990e39e3037abe857bc1b0cdfb695905a61439c', 'M', '41197732', 'G', 'A', 'M-41197732-G-A'])
-		self.assertEqual(process_variant_input(input6), ['e95a1742404bcc3c3c8f2ee44696b792a0cb634688a4349fa65195f5871ca027', 'Y', '41197732', 'G', 'A', 'Y-41197732-G-A'])
-		self.assertEqual(process_variant_input(input7), ['cb5a43b1cc81e1ca7aaf6765ed8f3b76de40c83c81923399779f56086ba31303', '13', '4', 'GTGAC', 'ATGC', '13-4-GTGAC-ATGC'])
+		self.assertEqual(process_variant_input(input1,ref1), ['e619796bddad0e522b9a127c3bea7906492173156fe5d566982ac06294a1c6b1', '17', '41197732', 'GGGGGATGC', 'ATGC', '17-41197732-GGGGGATGC-ATGC'])
+		self.assertEqual(process_variant_input(input2,ref2), ['abc7c2260d6991379ca3bafb2ce7a5381e14161136b9ff26742491d3761f4947', '17', '41197732', 'AAAAG', 'ATTTTT', '17-41197732-AAAAG-ATTTTT'])
+		self.assertEqual(process_variant_input(input3,ref1), ['1803ac07aaa1a21494fde4ddf6b2f154ac569e2dbb195c0d4428b21a74580e73', '1', '41197732', 'G', 'A', '1-41197732-G-A'])
+		self.assertEqual(process_variant_input(input4,ref2), ['95394400756ce41824f8a728f87722051f7d105aaa69f22955f535f660caa037', 'X', '41197732', 'G', 'AAA', 'X-41197732-G-AAA'])
+		self.assertEqual(process_variant_input(input5,ref1), ['8e6e0816a10135d2d295f2b42990e39e3037abe857bc1b0cdfb695905a61439c', 'M', '41197732', 'G', 'A', 'M-41197732-G-A'])
+		self.assertEqual(process_variant_input(input6,ref2), ['fead1b193511ee17dd92c6a554f664027005c7a5af5fd19536bfca7b338f2951', 'Y', '41197732', 'G', 'A', 'Y-41197732-G-A'])
+		self.assertEqual(process_variant_input(input7,ref1), ['cb5a43b1cc81e1ca7aaf6765ed8f3b76de40c83c81923399779f56086ba31303', '13', '4', 'GTGAC', 'ATGC', '13-4-GTGAC-ATGC'])
 
 
 
