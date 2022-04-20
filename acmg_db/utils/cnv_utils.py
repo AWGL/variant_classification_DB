020525cc6a1478019a5492e103799418228e7ac9
Sophie Shaw <sophie.shaw@wales.nhs.uk>
2022-04-20 14:13:14 +0100

    Unit test for calculate acmg score util

--- acmg_db/utils/cnv_utils.py
+++ acmg_db/utils/cnv_utils.py
@@ -267,21 +267,22 @@ def get_vep_info_local_cnv(cnv_list, vep_info, sample):
 def calculate_acmg_class(score):
 	
 	print(score)
-	score = float(score) #bug fix, without this the elif statements below don't work correctly
 	if score == "NA":
 		return "5"
-	elif score >= 0.99:
-		return "4"
-	elif 0.90 <= score <= 0.98: 
-		return "3"
-	elif -(0.89) <= score <= 0.89:
-		return "2"
-	elif -(0.98) <= score <= -(0.90):
-		return "1"
-	elif score <= -(0.99):
-		return "0"
 	else:
-		print("ERROR! Class could not be calculated")
+		score = float(score) #bug fix, without this the elif statements below don't work correctly
+		if score >= 0.99:
+			return "4"
+		elif 0.90 <= score <= 0.98: 
+			return "3"
+		elif -(0.89) <= score <= 0.89:
+			return "2"
+		elif -(0.98) <= score <= -(0.90):
+			return "1"
+		elif score <= -(0.99):
+			return "0"
+		else:
+			print("ERROR! Class could not be calculated")
 	
 		
 		
